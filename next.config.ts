247afa2f4bf80c28722be2d71a5c24233dadccf9
Andres Arzamendia <arzamendiaandres@gmail.com>
2026-05-26 21:22:42 -0300

    Agregar soporte webpack para mapas en next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Evita que las librerías de mapas rompan la compilación del cliente
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
